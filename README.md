2a7a9aea051a1245d2329461d00e105a2b12ea45
Satoshi Watanabe <sassy.watanabe@gmail.com>
2016-07-01 09:44:24 +0900

    first commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# dccs (Delay Certificate of JR Chuo-Sobu Line)
+
+## usage
+
+```
+$ npm install -g dccs
+$ dccs
+```
---
+++ main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const url = require('url');
+const opener = require("opener");
+const ncp = require("copy-paste");
+
+const now = new Date();
+const year = String(now.getFullYear());
+const month = ('0' + (now.getMonth() + 1)).slice(-2);
+const date = ('0' + now.getDate()).slice(-2);
+
+const requestUrl = url.format({
+  protocol: 'http',
+  hostname: 'traininfo.jreast.co.jp',
+  pathname: 'delay_certificate/pop.aspx',
+  query: {
+    D: year + month + date,
+    R: '07'
+  }
+});
+
+ncp
+opener(requestUrl);
+ncp.copy(requestUrl, function () {
+  console.log(requestUrl);
+});
